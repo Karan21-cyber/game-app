52b09308719e88fb0434418af838f8602638be9a
Karan Chaudhary <tharukaran930@gmail.com>
2023-08-22 22:59:43 +0545

    fix bug

--- src/components/product-category/category-page.tsx
+++ src/components/product-category/category-page.tsx
@@ -35,7 +35,7 @@ function CategoryPage({ params }: { params: { genres: string } }) {
 
   useEffect(() => {
     fetchData();
-  });
+  },[]);
 
   return (
     <section className="category-page py-2 px-2 md:px-4 md:py-6 lg:px-[70px] flex flex-col md:flex-row gap-4  ">
--- src/components/product-category/product-details.tsx
+++ src/components/product-category/product-details.tsx
@@ -6,37 +6,45 @@ import React, { useState, useEffect } from "react";
 import { BsFillStarFill } from "react-icons/bs";
 import MovieLoading from "./movie-loading";
 
+export interface Results {
+  imdb_id: string;
+  title: string;
+  year: number;
+  popularity: number;
+  description: string;
+  content_rating: string;
+  movie_length: number;
+  rating: number;
+  created_at: Date;
+  trailer: string;
+  image_url: string;
+  release: string;
+  plot: string;
+  banner: string;
+  type: string;
+  more_like_this: MoreLikeThis;
+  gen: Gen[];
+  keywords: Keyword[];
+}
+
+interface Gen {
+  id: number;
+  genre: string;
+}
+
+interface Keyword {
+  id: number;
+  keyword: string;
+}
+
+interface MoreLikeThis {}
+
 interface IMovieDetail {
-  movie: {
-    imdb_id: string;
-    title: string;
-    year: number;
-    popularity: number;
-    description: string;
-    content_rating: string;
-    movie_length: number;
-    rating: number;
-    created_at: string;
-    trailer: string;
-    image_url: string;
-    release: string;
-    plot: string;
-    banner: string;
-    type: string;
-    gen: {
-      id: number;
-      genre: string;
-    }[];
-    keyword: {
-      id: number;
-      keyword: string;
-    }[];
-  };
   pid?: string;
 }
 
-function ProductDetails({ pid }: { pid: string }) {
-  const [movie, setMovie] = useState<IMovieDetail>();
+function ProductDetails({ pid }: IMovieDetail) {
+  const [movies, setMovie] = useState<Results>();
   const [loading, setLoading] = useState(true);
 
   const fetchData = async () => {
@@ -53,8 +61,9 @@ function ProductDetails({ pid }: { pid: string }) {
 
       const response = await axios.request(options);
       if (response) {
-        const res = response.data.results as IMovieDetail;
-        setMovie({movie:res} );
+        const res = response.data.result;
+        console.log(res);
+        setMovie(res);
         setLoading(false);
       }
     } catch (error) {
@@ -64,7 +73,7 @@ function ProductDetails({ pid }: { pid: string }) {
 
   useEffect(() => {
     fetchData();
-  });
+  }, []);
 
   return (
     <section className="product-details-container py-2 px-2 md:px-4 md:py-6 lg:px-[70px] flex flex-col gap-3 lg:gap-10  ">
@@ -73,45 +82,43 @@ function ProductDetails({ pid }: { pid: string }) {
       ) : (
         <div className="product-details-wrapper flex flex-col lg:flex-row gap-10 ">
           <Image
-            src={movie?.movie?.banner as string} // Provide a default image URL or use a placeholder image
+            src={movies?.banner as string} // Provide a default image URL or use a placeholder image
             width={500}
             height={500}
-            alt={movie?.movie?.title as string}
+            alt={movies?.title as string}
             className="product-image"
           />
           <div className="product-detail-information w-full flex flex-col gap-5">
             <div className="product-highlight  w-full flex justify-between">
               <h1 className="product-title text-2xl font-bold text-light-gray">
-                {movie?.movie?.title}
+                {movies?.title}
               </h1>
               <h1 className="product-title text-2xl font-bold text-light-gray flex gap-2">
-                {movie?.movie?.rating}
+                {movies?.rating}
                 <BsFillStarFill className="star w-6 h-6 text-dark-yellow" />
               </h1>
             </div>
             <div className="product-detail-times flex ">
               <h1 className="produt-information  text-gray-500 pr-4  border-r-2 border-gray-600">
-                {movie?.movie.year}
+                {movies?.year}
               </h1>
               <h1 className="produt-information  text-gray-500 px-4  border-r-2 border-gray-600">
-                {movie?.movie.movie_length} min
+                {movies?.movie_length} min
               </h1>
               <h1 className="produt-information  text-gray-500 px-4">
-                {movie?.movie.popularity} +
+                {movies?.popularity} +
               </h1>
             </div>
             <p className="product-description text-gray-400">
-              {movie?.movie.description}
+              {movies?.description}
             </p>
-            <p className="product-description text-gray-400">
-              {movie?.movie.plot}
-            </p>
+            <p className="product-description text-gray-400">{movies?.plot}</p>
             <h1 className="product-release-date text-gray-300">
-              Release Date : {movie?.movie.release}
+              Release Date : {movies?.release}
             </h1>
             <div className="product-category flex flex-wrap gap-4">
               Movie Categories{" "}
-              {movie?.movie.gen?.map((category, i) => (
+              {movies?.gen?.map((category, i) => (
                 <h1 className="category text-gray-400" key={i}>
                   {category?.genre}
                 </h1>
@@ -123,7 +130,7 @@ function ProductDetails({ pid }: { pid: string }) {
               </h1>
               <iframe
                 className="youtube-trailer w-full h-[250px] md:w-[560px] md:h-[315px] mt-4"
-                src={`${movie?.movie?.trailer}?autoplay=1&mute=1`}
+                src={`${movies?.trailer}?autoplay=1&mute=1`}
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               ></iframe>
